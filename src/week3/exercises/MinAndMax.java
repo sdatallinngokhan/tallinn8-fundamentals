728ca7a06dd128e7d59679b8a65b3414dd22700c
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-07-27 11:18:40 +0300

    min and max example

---
+++ src/week3/exercises/MinAndMax.java
@@ -0,0 +1,24 @@
+package week3.exercises;
+
+public class MinAndMax {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 5, 3, 8, 9, 1, 7, 0};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println("Min number : " + min);
+        System.out.println("Max number : " + max);
+    }
+}
