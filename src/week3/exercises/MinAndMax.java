9c77dde714c0e739d865c59a324b918d95e05425
gokhanpolat <gokhanpolat203@aof.anadolu.edu.tr>
2019-07-27 11:19:50 +0300

    min and max example exp

--- src/week3/exercises/MinAndMax.java
+++ src/week3/exercises/MinAndMax.java
@@ -1,3 +1,10 @@
+//Find smallest and biggest element of an array
+//
+//int[] arr = {5, 2, 5, 3, 8, 9, 1, 7, 0};
+//
+//smallest : 0
+//biggest : 9
+
 package week3.exercises;
 
 public class MinAndMax {
